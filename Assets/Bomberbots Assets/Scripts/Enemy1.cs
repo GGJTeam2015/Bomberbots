9133dd4c36b5681634c2916f09e4c8ca53d5aa11
Selcuk Ozturk <selcukahmed@gmail.com>
2015-01-24 03:20:17 +0200

    Enemy added

---
+++ Assets/Bomberbots Assets/Scripts/Enemy1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviourExtend
+{
+    [SerializeField] private float chaseRange = 10.0f;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawWireSphere(CachedTransform.position, chaseRange);
+    }
+}
