0415dd2524fb12ddae6d2daf320a2a9e0af7efaa
Selcuk Ozturk <selcukahmed@gmail.com>
2015-01-24 02:45:14 +0200

    Spawner added

---
+++ Assets/Bomberbots Assets/Scripts/MonoBehaviourExtend.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MonoBehaviourExtend : MonoBehaviour
+{
+    private Transform cachedTransform = null;
+    private Rigidbody cachedRigidbody = null;
+
+    public Transform CachedTransform
+    {
+        get { return cachedTransform ?? (cachedTransform = transform); }
+    }
+
+    public Rigidbody CachedRigidbody
+    {
+        get { return cachedRigidbody ?? (cachedRigidbody = rigidbody); }
+    }
+}
---
+++ Assets/Bomberbots Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviourExtend
+{
+    [SerializeField]
+    private float movementSpeed = 1;
+
+    [SerializeField]
+    private Vector3 direction = Vector3.zero;
+
+	void Start () 
+    {
+	
+	}
+	
+	void LateUpdate () 
+    {
+        Vector3 velocity = direction.normalized * movementSpeed;
+        velocity.y = CachedRigidbody.velocity.y;
+        CachedRigidbody.velocity = velocity;
+
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            CachedTransform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+	}
+}
