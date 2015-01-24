0a7fa5094892a53262758a74b19902fcace216ef
KemalTy <y.kemal.tugrul@gmail.com>
2015-01-24 03:34:04 +0200

    Safe Zone Behavior Added
    ver 1.0

--- Assets/Bomberbots Assets/Scripts/followObject.cs
+++ Assets/Bomberbots Assets/Scripts/followObject.cs
@@ -4,31 +4,31 @@ using System.Collections;
 public class followObject : MonoBehaviour {
 
 	public Transform objectToFollow;
-    public float smoothness;
+	public float smoothness;
 
-    private Vector3 initialDistance = new Vector3(0.0f, 0.0f, 0.0f);
+	private Vector3 initialDistance = new Vector3(0.0f, 0.0f, 0.0f);
 
 	// Use this for initialization
 	void Start () {
 
-        initialDistance = objectToFollow.transform.position - this.transform.position;
-        
+		initialDistance = objectToFollow.transform.position - this.transform.position;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Determine point to go
-        Vector3 pointToGo = objectToFollow.transform.position - initialDistance;
+		// Determine point to go
+		Vector3 pointToGo = objectToFollow.transform.position - initialDistance;
 
-        // Look at object
-        this.transform.LookAt(objectToFollow);
+		// Look at object
+		this.transform.LookAt(objectToFollow);
 
-        // Follow object
-        float t =  Vector3.Distance(this.transform.position, pointToGo) / smoothness;
-        this.transform.position = Vector3.Lerp(this.transform.position, pointToGo, t);
+		// Follow object
+		float t =  Vector3.Distance(this.transform.position, pointToGo) / smoothness;
+		this.transform.position = Vector3.Lerp(this.transform.position, pointToGo, t);
 
 	}
 
-    
+	
 }
---
+++ Assets/Bomberbots Assets/Scripts/safeZoneBehaviour.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class safeZoneBehaviour : MonoBehaviour {
+
+	// List for craft piece lists
+    public List<string> craftPieceTags = new List<string>(5);
+
+    // Parameters for random gen. goal
+    public int totalNumOfObj;
+
+
+    // Counts for craft pieces
+    public int[] numOfItemNeeded;
+
+	// Use this for initialization
+	void Start () {
+
+        // Initialize num of items needed
+        numOfItemNeeded = new int[craftPieceTags.Count];
+        for (int i = 1; i != numOfItemNeeded.Length; i++ )
+        {
+            numOfItemNeeded[i] = 0;
+        }
+
+        // Generate goals
+        genGoals();
+	}
+
+
+
+
+    // Fill *numOfItemNeeded* for generating random goals
+    void genGoals()
+    {
+        int counta = 0;
+
+        while (true)
+        {
+            int idx = Random.Range(0, numOfItemNeeded.Length);
+            int count = Random.Range(0, totalNumOfObj - sumArray(numOfItemNeeded) + 1);
+            numOfItemNeeded[idx] += count;
+
+
+            if (sumArray(numOfItemNeeded) >= totalNumOfObj || counta > 500)
+            {
+                break;
+            }
+
+            counta++;
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    int sumArray(int[] array)
+    {
+        int sum = 0;
+        for(int i = 0; i != array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+
+    // Check if goal is reached!
+    bool checkEndCondition()
+    {
+        bool isCondReached = true;
+
+        for (int i = 1; i != numOfItemNeeded.Length; i++ )
+        {
+            if(numOfItemNeeded[i] > 0)
+            {
+                isCondReached = false;
+            }
+        }
+
+        return isCondReached;
+    }
+
+    // Check if craft objects entered the safe zone
+    void OnTriggerEnter(Collider coll) 
+    {
+        string tagOfCollider = coll.gameObject.tag;
+
+        if (craftPieceTags.Exists(x => x == tagOfCollider))
+        {
+            // Increment Count
+            int idx = craftPieceTags.FindIndex(x => x == tagOfCollider);
+            numOfItemNeeded[idx]--;
+
+            // Destroy other object
+            // Destroy(coll.gameObject);
+
+            // Check end condition
+            checkEndCondition();
+        }
+    }
+
+
+    // ACCESSORS(GETTERS)
+    public List<string> getCraftPieceTags()
+    {
+        return craftPieceTags;
+    }
+
+    public int[] getNumOfPiecesNeeded()
+    {
+        return numOfItemNeeded;
+    }
+
+    
+}
